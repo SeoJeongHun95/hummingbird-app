a0724bbc43b74c989e80c2b7beb71b4b07832708
SEO <jpssjjhh@gmail.com>
2025-01-06 19:34:16 +0900

    Feat: Implement home screen with segmented tabs and associated screens

---
+++ lib/src/views/home/home_seg3_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+//요약
+class Seg3Screen extends StatelessWidget {
+  const Seg3Screen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("se3"),
+    );
+  }
+}
