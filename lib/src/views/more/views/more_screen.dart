94670cd427f365cd9d9b805198b457515abfd5a5
Maccrey <m01071630214@gmail.com>
2025-01-30 23:56:29 +0900

    review 버튼 settingScreen에서 넣어 놓음

--- lib/src/views/more/views/more_screen.dart
+++ lib/src/views/more/views/more_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../../../core/router/bottom_nav_bar.dart';
+import '../../../providers/review/review_provider.dart';
 import '../widgets/options_container_widget.dart';
 import '../widgets/user_auth_widget.dart';
 import '../widgets/user_profile_widget.dart';
@@ -11,6 +12,7 @@ class MoreScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final reviewService = ref.read(reviewProvider.notifier);
     return Scaffold(
       appBar: AppBar(),
       body: SafeArea(
@@ -20,6 +22,10 @@ class MoreScreen extends ConsumerWidget {
               UserProfileWidget(),
               OptionsContainerModule(),
               UserAuthWidget(),
+              TextButton(
+                onPressed: () => reviewService.requestReview(context),
+                child: const Text('리뷰 하기'),
+              ),
             ],
           ),
         ),
