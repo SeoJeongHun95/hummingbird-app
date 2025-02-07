1618feb9651ceefcb2ec19c253f582e1453ea122
SEO <jpssjjhh@gmail.com>
2025-02-07 20:39:13 +0900

    Revert "Feat : Add gaimon dependency and integrate Gaimon selection into SuDuckTimer"
    This reverts commit ef24c351baa82683bfde7fbca4a4de094a64bb58.

--- lib/src/providers/suduck_timer/suduck_timer_provider_2_0.dart
+++ lib/src/providers/suduck_timer/suduck_timer_provider_2_0.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 
-import 'package:gaimon/gaimon.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 import '../../../core/utils/utils.dart';
@@ -68,8 +67,6 @@ class SuDuckTimer extends _$SuDuckTimer {
   Future<void> startTimer({Subject? subject}) async {
     final isConnected = ref.watch(networkStateProvider);
     if (isConnected.asData?.value != true) return;
-
-    Gaimon.selection();
 
     if (state.isRunning) return;
     _cancelBreakTimer();
