3c2d416455d4df650a84a6797336d1fe0cecb24d
Seok hoon Lim <m01071630214@gmail.com>
2025-02-27 23:52:17 +0900

    Update fcm.dart
    임시 수정

--- lib/core/utils/fcm.dart
+++ lib/core/utils/fcm.dart
@@ -86,7 +86,11 @@ class FcmManager {
 
     // APNS 토큰 획득 시도
     int attempts = 0;
-    const maxAttempts = 5;
+          // const maxAttempts = 3;
+
+      // 임시로 1회
+      const maxAttempts = 1;
+    
 
     while (attempts < maxAttempts) {
       try {
