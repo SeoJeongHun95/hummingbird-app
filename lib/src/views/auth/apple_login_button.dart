89553d7d55223dadad66bf67769a2ee57e808441
SEO <jpssjjhh@gmail.com>
2025-03-18 20:08:45 +0900

    feat: Add entitlements for Apple Sign In support

--- lib/src/views/auth/apple_login_button.dart
+++ lib/src/views/auth/apple_login_button.dart
@@ -3,30 +3,30 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class GoogleLoginButton extends ConsumerWidget {
-  const GoogleLoginButton({super.key});
+class AppleLoginButton extends ConsumerWidget {
+  const AppleLoginButton({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return ElevatedButton.icon(
       onPressed: () async =>
-          await ref.read(authProvider.notifier).signInWithGoogle(),
+          await ref.read(authProvider.notifier).signInWithApple(),
       label: Text(
-        "Sign in with Google",
+        " Sign in with Apple ",
         style: TextStyle(
-          color: Colors.black,
+          color: Colors.white,
           fontSize: 14.sp,
           fontWeight: FontWeight.w600,
         ),
       ),
       icon: Image.asset(
-        "lib/core/imgs/icons/google_logo.png",
+        "lib/core/imgs/icons/apple_logo_black.png",
         height: 40.h,
         width: 40.h,
       ),
       style: ElevatedButton.styleFrom(
         fixedSize: Size.fromWidth(240.w),
-        backgroundColor: Color(0xffF2F2F2),
+        backgroundColor: Colors.black,
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(8),
           side: BorderSide(color: Colors.black, width: 1),
