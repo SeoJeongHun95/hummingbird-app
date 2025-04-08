ac7db8c716114eede9307c639476600eb80dbda1
SEO <jpssjjhh@gmail.com>
2025-04-09 01:19:09 +0900

    Refactor: Simplify date calculation in getLeaderboard method

--- lib/src/repositories/leader_board/leader_board_repository.dart
+++ lib/src/repositories/leader_board/leader_board_repository.dart
@@ -11,17 +11,14 @@ class LeaderboardRepository {
   final LeaderboardRemoteDatasource _remoteDataSource;
 
   LeaderboardRepository(this._remoteDataSource);
-
   Future<Map<String, dynamic>> getLeaderboard() async {
-    DateTime now = DateTime.now().subtract(const Duration(hours: 6));
-
-    DateTime targetDate = now.hour < 6
-        ? now.subtract(Duration(days: 1))
-        : now.subtract(Duration(days: 2));
-
-    final dateKey = formatDate(targetDate);
-
-    final result = await _remoteDataSource.getLeaderboard(dateKey);
+    final result = await _remoteDataSource.getLeaderboard(
+      formatDate(
+        DateTime.now()
+            .subtract(const Duration(hours: 6))
+            .subtract(Duration(days: 1)),
+      ),
+    );
     return result;
   }
 }
